4740759f2011c898c04ccd230e08530deb6fa5ef
OAPadillaHerrera <oscar.padilla.ha@gmail.com>
2024-10-30 21:03:19 -0500

    Commit, SUP, Oscar.

---
+++ src/Components/Card.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+import styles from "../Components/Card.module.css";
+
+export const Card = () => {
+
+  return (
+
+    <div className = {styles.cardContainer}>Card</div>
+
+  )
+
+}
+
+export default Card;
